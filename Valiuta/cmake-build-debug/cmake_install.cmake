39a253549f1ab28df1fc83f1a4cc441ea1d60521
Drogitronas <modestasblackus@gmail.com>
2024-10-10 10:49:26 +0300

    Didzioji permaina

--- Valiuta/cmake-build-debug/cmake_install.cmake
+++ Valiuta/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Mode/Desktop/Strukt
+# Install script for directory: C:/Users/Mode/Desktop/Strukt/Valiuta
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Mode/Desktop/Strukt/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Mode/Desktop/Strukt/Valiuta/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
