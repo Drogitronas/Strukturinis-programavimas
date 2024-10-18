55dd0a94b64888bd4fe1e48547f5cecab2d157d8
Drogitronas <modestasblackus@gmail.com>
2024-10-18 19:12:57 +0300

    Pabaigtas zaidimas

--- Zaidimai/Zaidimai.cpp
+++ Zaidimai/Zaidimai.cpp
@@ -4,20 +4,30 @@
 #include <cstdlib>
 
 using namespace std;
-void Raides (int &pasirinkimas);
+
+void Raides (int &pasirinkimas, char raide);
 void Euklidas (int skaicius1, int skaicius2, int &pasirinkimas, int dbd );
 void Skaiciaus_spejimas (int &pasirinkimas);
 void fizzbuzz (int &pasirinkimas, int n);
 int main() {
-    int skacius1, skaicius2, pasirinkimas, dbd, n;
+    int skaicius1, skaicius2, pasirinkimas, dbd, n;
+    char raide;
+    string pabaiga;
+cout << "Galite sustabdyti programa, parasydami 5 ." << endl;
+    cout << endl;
+    cout << endl;
+
+while (true) {
     printf("Meniu:\n");
     printf("1. Raides\n");
     printf("2. Euklidas\n");
     printf("3. Skaiciaus spejimas\n");
     printf("4. Fizzbuzz\n");
+    printf("5. iseiti\n");
     printf("iveskite savo pasirinkima: ");
     scanf("%d", &pasirinkimas);
 
+    cout << endl;
     switch (pasirinkimas) {
         case 1:
             printf("Jus pasirinkote raidziu zaidima.\n");
@@ -28,26 +38,32 @@ int main() {
         break;
         case 3:
             printf("Jus pasirinkote spejimo zaidima \n");
+        break;
         case 4:
             printf("Jus pasirinkote fizzbuzz zaidima \n");
+        break;
+        case 5:
+            printf("Jus isejote is programos\n");
         break;
         default:
             printf("Tokio pasirinkimo nera");
     }
-
-    Raides(pasirinkimas);
-    Euklidas(skacius1,skaicius2,pasirinkimas,dbd);
+cout << endl;
+    Raides(pasirinkimas,raide);
+    Euklidas(skaicius1,skaicius2,pasirinkimas,dbd);
     Skaiciaus_spejimas(pasirinkimas);
     fizzbuzz(pasirinkimas,n);
 
-
+    if (pasirinkimas == 5) {
+        break;
+    }
+}
 
 return 0;
     }
-    void Raides (int &pasirinkimas) {
+    void Raides (int &pasirinkimas, char raide) {
     if (pasirinkimas ==1) {
         cout << pasirinkimas << endl;
-        char raide;
         cout << "Iveskite raide:" << endl;
         cin >> raide;
         if (raide == 'A' || raide == 'a' || raide == 'Ą' || raide == 'ą' ||
@@ -66,11 +82,15 @@ cout << "Yveskite pirmus skaicius"  << endl;
     cin >> skaicius1;
     cout << "Yveskite antrus skacius" << endl;
     cin >> skaicius2;
-    while (abs(skaicius1) && abs(skaicius2))
-        if (abs(skaicius1) > abs(skaicius2)) skaicius1 %= skaicius1;
-        else skaicius2 %= skaicius1;
+    while (abs(skaicius1) != 0 && abs(skaicius2) != 0) {
+        if (abs(skaicius1) > abs(skaicius2)) {
+            skaicius1 %= skaicius2;
+        } else {
+            skaicius2 %= skaicius1;
+        }
+    }
     dbd = skaicius1 + skaicius2;
-cout << dbd;
+cout << dbd << endl;
 }
 }
 void Skaiciaus_spejimas (int &pasirinkimas) {
@@ -99,10 +119,12 @@ void fizzbuzz (int &pasirinkimas, int n) {
         cout << "Yveskite teigiama kintamajy skaiciu: ";
         cin >> Skaicius;
         for (int i = 1; i <= Skaicius; i++) {
-            cout << i << endl;
-            if(i % 3 ==0) cout << "Fizz" << endl;
-            if(i % 5 ==0) cout << "Buzz" << endl;
-            if(i % 3 ==0 && i % 5 ==0) cout << "Fizzbuzz" << endl;
+            //cout << i;
+            if (i % 3 ==0 && i % 5 ==0) {
+                cout << "Fizzbuzz" << endl;
+
+            }if (i % 3 ==0) {cout << "Fizz" << endl;
+        }if (i % 5 ==0) {cout << "Buzz" << endl;}else cout << i << endl;
         }
 
     }
